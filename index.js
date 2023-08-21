f7551c57c624811e4efb8cbb674f90ba8b1e37c5
Shahariyar123 <mdshahariyarjaman4291@gmail.com>
2023-08-21 20:00:50 +0600

    3rd commit

--- index.js
+++ index.js
@@ -1,41 +1,62 @@
-let total = 0;   
-   
-   function getAllMoney(target){
-    // console.log('je')
-    const secletedItmes =document.getElementById('seclect-item')
-const itemNames = target.parentNode.childNodes[3].innerText
-const ul= document.createElement("ul")
-ul.innerText = itemNames;
-// console.log(li);
-secletedItmes.appendChild(ul)
- 
-// console.log(target.parentNode.childNodes[5].innerText.split(" ")[0])
+let serialNumber = 1; // Initialize serial number
+let total = 0;
 
-const price =target.parentNode.childNodes[5].innerText.split(" ")[0];
+function getAllMoney(target) {
+  const selectedItems = document.getElementById('select-item');
+  const itemNames = target.parentNode.childNodes[3].innerText;
+  const li = document.createElement("li");
+  li.innerText = `${serialNumber}. ${itemNames}`;
+  serialNumber++;
+  // Append the list item to the selectedItems list
+  selectedItems.appendChild(li);
+  const price = parseFloat(target.parentNode.childNodes[5].innerText.split(" ")[0]);
+  
+  total += price;
+  const totalPriceElement = document.getElementById('Total-price');
+  totalPriceElement.innerText = total;
 
-total = parseFloat(total) + parseFloat(price)
-
-// console.log(total);
-const totalPrice = (document.getElementById('Total-price').innerText = total)
-
-
-
- // Example: Applying a discount if the price value is less than or equal to 200
-let prices = 0;
-if (prices <= 200) {
-  const discount = (totalPrice*20)/100
- const totalDis = discount
-const discountTotal = (document.getElementById('discount-total').innerText = totalDis)
-console.log('total')
-const count =total-discount;
-const mainPrice = (document.getElementById('main-price').innerText =  count )
-} 
-function resetAllMoney(ih){
-  console.log('hosce')
+  applyDiscountIfNeeded();
 }
- };
 
 
+function applyDiscountIfNeeded() {
+  const totalPrice = parseFloat(document.getElementById('Total-price').innerText);
+
+  if (totalPrice >= 200 && isCouponApplied()) {
+    const discount = (totalPrice * 20) / 100;
+    const discountedTotal = totalPrice - discount;
+
+    const discountTotalElement = document.getElementById('discount-total');
+    discountTotalElement.innerText = discount.toFixed(2);
+
+    const mainPriceElement = document.getElementById('main-price');
+    mainPriceElement.innerText = discountedTotal.toFixed(2);
+  } else {
+    const discountTotalElement = document.getElementById('discount-total');
+    discountTotalElement.innerText = "0.00";
+
+    const mainPriceElement = document.getElementById('main-price');
+    mainPriceElement.innerText = totalPrice.toFixed(2);
+  }
+}
+
+function isCouponApplied() {
+  const couponInput = document.getElementById('coupon-input').value;
+  return couponInput === "SELL200";
+}
+
+function resetAllMoney() {
+  const selectedItems = document.getElementById('select-item');
+  selectedItems.innerHTML = '';
+
+  const totalPriceElement = document.getElementById('Total-price');
+  totalPriceElement.innerText = "0.00";
+
+  const couponInput = document.getElementById('coupon-input');
+  couponInput.value = "";
+
+  applyDiscountIfNeeded();
+}
 
 
 
